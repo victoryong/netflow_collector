45245ab5de97cd6f5fe637b6d621a2c64ed98d0c
victoryong <victor241@163.com>
2020-08-13 16:46:17 +0800

    ok

--- clean_files.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-rm -f /home/victor/GitRepo/flow_dir/*
-rm -f /home/victor/GitRepo/nd_dir/*
-
-cd /usr/hadoop/hadoop-3.0.0/
-bin/hadoop fs -rm /user/hadoop/flow_txt/*
-
-
--- kill_capd.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-port_report=`netstat -anp | grep 9995`
-port_status=$(echo $port_report | grep -E -o '[[:digit:]]+\/nfcapd')
-if [[ $port_status != '' ]]; then
-	pid=${port_status//\/nfcapd/''}
-	kill -9 $pid
-fi
-
